b32e14b00743b42d1ad87ddabb9b1012758933bb
jaehwanrho <jack8023@naver.comJaeHwancBookAir:Visual Studio Code jaehwanrho$ git config --global user.email jack8023@naver.com>
2020-09-29 14:33:04 +0900

    SEP29143300

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resourcessssssssss');
+  res.send('respond with a resourcessssssssssssssss');
 });
 
 module.exports = router;
